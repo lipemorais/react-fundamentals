90c51b96761992338fcd6dd939777405e713c89b
Felipe de Morais <felipejpa15@gmail.com>
2016-05-02 19:40:49 -0300

    class 7 react fundamentals

--- App.js
+++ App.js
@@ -1,8 +1,50 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      red: 0,
+      green: 0,
+      blue: 0
+    }
+
+    this.update = this.update.bind(this)
+  }
+
+  update(evnt) {
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.input).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.input).value,
+      blue:  ReactDOM.findDOMNode(this.refs.blue.refs.input).value,
+    })
+  }
+
   render() {
-    return <div> Hello world</div>
+    return (
+      <div>
+
+        {this.state.txt}
+        <hr />
+        <Slider ref="red" update={this.update} />{this.state.red}
+        <Slider ref="green" update={this.update} />{this.state.green}
+        <Slider ref="blue" update={this.update} />{this.state.blue}
+      </div>
+    )
+  }
+}
+class Slider extends React.Component {
+
+  render() {
+    return (
+      <div>
+      <input ref="input" type="range"
+        min="0"
+        max="255"
+        onChange={this.props.update} />
+      </div>
+    )
   }
 }
 
