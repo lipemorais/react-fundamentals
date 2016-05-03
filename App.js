9f7ee81c673cf4f39e0ab386310d8b4a0f0b32d8
Felipe de Morais <felipejpa15@gmail.com>
2016-05-03 18:05:23 -0300

    class 8 react fundamentals

--- App.js
+++ App.js
@@ -1,51 +1,21 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 
 class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      red: 0,
-      green: 0,
-      blue: 0
-    }
-
-    this.update = this.update.bind(this)
-  }
-
-  update(evnt) {
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.input).value,
-      green: ReactDOM.findDOMNode(this.refs.green.refs.input).value,
-      blue:  ReactDOM.findDOMNode(this.refs.blue.refs.input).value,
-    })
-  }
-
   render() {
     return (
-      <div>
-
-        {this.state.txt}
-        <hr />
-        <Slider ref="red" update={this.update} />{this.state.red}
-        <Slider ref="green" update={this.update} />{this.state.green}
-        <Slider ref="blue" update={this.update} />{this.state.blue}
-      </div>
+      <Button>I <Heart /> React </Button>
     )
   }
 }
-class Slider extends React.Component {
 
+class Button extends React.Component {
   render() {
     return (
-      <div>
-      <input ref="input" type="range"
-        min="0"
-        max="255"
-        onChange={this.props.update} />
-      </div>
+      <button>{this.props.children}</button>
     )
   }
 }
+
+const Heart = () => <span className="glyphicon glyphicon-heart"> {'<3'} </span>
 
 export default App
